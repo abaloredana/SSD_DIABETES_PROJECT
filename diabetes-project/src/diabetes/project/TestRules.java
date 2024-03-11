d98a1120b35fe301a84fe08646c555e35b88a35b
Beatriz León García <78873722+beatrizleongarcia@users.noreply.github.com>
2024-03-11 16:43:51 +0100

    dos test de prueba

---
+++ diabetes-project/src/diabetes/project/TestRules.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diabetes.project;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+
+/**
+ *
+ * @author beatr
+ */
+
+public class TestRules {
+
+    @Test
+    public void testInsulin1Rule() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieSession ksession = kc.newKieSession("diabetesSession");
+
+        Patient patient = new Patient("Patient1", 1, 25, 23, 1, false, false, true, true, false, false);
+        ksession.insert(patient);
+        int firedRules = ksession.fireAllRules();
+        ksession.dispose();
+
+        assertEquals(1, firedRules);
+        assertEquals(1, patient.getTreatments().size());
+        assertEquals("Insulin", patient.getTreatments().iterator().next().getName());
+        
+        System.out.println("Test testInsulin1Rule is correct.");
+    }
+
+    @Test
+    public void testMetformin1Rule() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieSession ksession = kc.newKieSession("diabetesSession");
+
+        Patient patient = new Patient("Patient2", 1, 25, 23, 3, false, false, true, true, false, false);
+        ksession.insert(patient);
+        int firedRules = ksession.fireAllRules();
+        ksession.dispose();
+
+        assertEquals(1, firedRules);
+        assertEquals(1, patient.getTreatments().size());
+        assertEquals("Metformin", patient.getTreatments().iterator().next().getName());
+        
+        System.out.println("Test testMetformin1Rule is correct.");
+    }
+
+
+
+}
